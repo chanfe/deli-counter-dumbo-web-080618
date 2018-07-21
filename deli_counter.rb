5fd781a7ba29e4fd471535497943386e1b05d83a
Felix Chan <felixchan29@gmail.com>
2018-07-21 00:59:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,33 +6,24 @@ def take_a_number(katz_deli, name)
   "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+def now_serving(katz_deli)
+  if katz_deli.length < 1 
+    "There is nobody waiting to be served!"
+  else
+    "Currently serving #{katz_deli.shift()}."
+  end
+
 def line(katz_deli)
   if katz_deli < 1 
     "The line is currently empty."
   end
+  line_string = 
   for i in 1..katz_deli.length do
     
   end
 end
 
-+function takeANumber(katzDeliLine, name){
-+  katzDeliLine.push(name);
-+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-+}
-+
-+function nowServing(katzDeliLine){
-+  if(katzDeliLine.length < 1){
-+    return "There is nobody waiting to be served!"
-+  }
-+  else{
-+    return `Currently serving ${katzDeliLine.shift()}.`
-+  }
-+}
-+
-+function currentLine(katzDeliLine){
-+  if(katzDeliLine.length < 1){
-+    return "The line is currently empty.";
-+  }
+
 +  var lineString = `The line is currently: 1. ${katzDeliLine[0]}`;
 +  for(let i = 1; i < katzDeliLine.length; i++){
 +    lineString += `, ${i+1}. ${katzDeliLine[i]}`;
