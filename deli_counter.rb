4991ec6c9b21e1abdfe0994861f69db5bf8ecfc7
Felix Chan <felixchan29@gmail.com>
2018-07-21 00:56:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 katz_deli = []
 
-def take_a_number(katz_deli)
-  katz_deli.push()
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
 
 def line(katz_deli)
   if katz_deli < 1 
@@ -13,32 +13,26 @@ def line(katz_deli)
   end
 end
 
-function takeANumber(katzDeliLine){
-  katzDeliLine.push(aNumber);
-  aNumber++;
-  return `Welcome! You are number ${katzDeliLine.length} in line.`;
-}
-
-function nowServing(katzDeliLine){
-  if(katzDeliLine.length < 1){
-    return "There is nobody waiting to be served!"
-  }
-  else{
-    return `Currently serving ${katzDeliLine.shift()}.`
-  }
-}
-
-function currentLine(katzDeliLine){
-  if(katzDeliLine.length < 1){
-    return "The line is currently empty.";
-  }
-  var lineString = `The line is currently: `;
-  for(let i = 0; i < katzDeliLine.length; i++){
-    lineString += `${i+1}. #${katzDeliLine[i]}`;
-    
-    if(i < katzDeliLine.length - 1){
-      lineString += `, `;
-    }
-  }
-  return lineString;
-}
++function takeANumber(katzDeliLine, name){
++  katzDeliLine.push(name);
++  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
++}
++
++function nowServing(katzDeliLine){
++  if(katzDeliLine.length < 1){
++    return "There is nobody waiting to be served!"
++  }
++  else{
++    return `Currently serving ${katzDeliLine.shift()}.`
++  }
++}
++
++function currentLine(katzDeliLine){
++  if(katzDeliLine.length < 1){
++    return "The line is currently empty.";
++  }
++  var lineString = `The line is currently: 1. ${katzDeliLine[0]}`;
++  for(let i = 1; i < katzDeliLine.length; i++){
++    lineString += `, ${i+1}. ${katzDeliLine[i]}`;
++  }
++  return lineString;
